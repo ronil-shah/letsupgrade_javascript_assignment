289db7794e3be4433e1c20e34f4286137799fa6e
ronil-shah <51937997+ronil-shah@users.noreply.github.com>
2020-07-15 12:35:53 +0530

    day 3 assignment

---
+++ day 3 ass/Day3_assessment.js
@@ -0,0 +1,14 @@
+alert("Welcome to day 3 assignment 1");
+console.log("hey I am a learning javascript");
+
+
+var a = {name:"Ronil", age:"22"}
+var b = {name:"Shah", age:"22"}
+
+console.time('Time taken');
+console.log("Hey")
+console.error("an error occured!");
+console.warn("Hey this is a warning!!");
+console.log({a ,b})
+console.table({a ,b})
+console.timeEnd('Time taken')
