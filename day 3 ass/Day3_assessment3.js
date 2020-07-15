1e00a5b873bb9ff0dd150bdc519117ba83462aff
ronil-shah <51937997+ronil-shah@users.noreply.github.com>
2020-07-15 12:40:03 +0530

    day 3 assignment 3

---
+++ day 3 ass/Day3_assessment3.js
@@ -0,0 +1,33 @@
+//string datatypes
+var a = 'Hi!';  
+var b = "Hello";
+//number datatypes
+var a = 25;        
+var b = 80.5;      
+var c = 4.25e+6;   
+var d = 4.25e-6;   
+
+//boolean datatypes
+var isReading = true;   
+var isSleeping = false; 
+
+//undefined datatypes
+var a;
+var b = "Hello World!"
+
+//object datatypes
+var emptyObject = {};
+
+var car = {
+    "modal": "BMW X3",
+    "color": "white",
+    "doors": 5
+}
+
+//array datatypes
+var colors = ["Red", "Yellow", "Green", "Orange"];
+
+//function datatypes
+var greeting = function(){ 
+    return "Hello World!"; 
+}
